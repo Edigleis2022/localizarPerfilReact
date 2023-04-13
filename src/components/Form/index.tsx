4b2b136eb6912f62980d7fc5f8d0851542d32d32
Edigleis <105567136+Edigleis2022@users.noreply.github.com>
2023-04-12 22:38:27 -0400

    💄 funçoes novas 12/04

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -2,15 +2,20 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 
+
 export default function BasicTextFields() {
-  return (
-    <Box
-    sx={{
-      width: 500,
-      maxWidth: '100%',
-    }}
-  >
-    <TextField fullWidth label="fullWidth" id="fullWidth" />
-  </Box>
-  );
+    return (
+        <Box
+            sx={{
+                width: 500,
+                maxWidth: '100%',
+                position:'relative',
+                justifyContent: 'center'
+
+            }}
+        >
+            <TextField fullWidth label="fullWidth" id="fullWidth" />
+            
+        </Box>
+    );
 }
